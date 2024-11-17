507a364c0eecce151a6b9b851b6a6421b9770e7e
Om Patel <om28patel2004@gmail.com>
2024-11-17 08:01:59 +0530

    feat: recursively delete folders

--- routes/files/delete-all.js
+++ routes/files/delete-all.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const File = require("../../models/FileSchema");
+const Folder = require("../../models/FolderSchema");
 
 router.delete("/:userID", async (req, res) => {
-  const userID = req.params.userID;
-  console.log(userID);
+  const { userID } = req.params;
+
   try {
     await File.deleteMany({ userID });
 
-    res.status(200).json({ message: "All files deleted successfully" });
+    const deleteFoldersRecursively = async (parentFolderID) => {
+      const folders = await Folder.find({ parentFolderID, userID });
+      for (const folder of folders) {
+        await deleteFoldersRecursively(folder._id);
+        await Folder.findByIdAndDelete(folder._id);
+      }
+    };
+
+    await deleteFoldersRecursively(null);
+
+    await Folder.deleteMany({ userID });
+
+    res
+      .status(200)
+      .json({ message: "All files and folders deleted successfully" });
   } catch (error) {
-    console.error("Error deleting files for user:", error);
-    res.status(500).json({ message: "Error deleting files", error });
+    console.error("Error deleting all files and folders:", error);
+    res
+      .status(500)
+      .json({ message: "Error deleting all files and folders", error });
   }
 });
 
